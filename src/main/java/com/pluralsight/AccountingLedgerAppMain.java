be90cf0171447f00ee95045f7d4c960e015f3745
Adrian <adrianchristiansanmiguel@gmail.com>
2024-10-11 11:54:02 -0700

    Initial Start

---
+++ src/main/java/com/pluralsight/AccountingLedgerAppMain.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+/**
+ * Hello world!
+ *
+ */
+public class AccountingLedgerAppMain
+{
+    public static void main( String[] args ) {
+        // add deposit
+
+        // make payment
+
+        // display ledger
+
+        // exit
+
+
+        System.out.println( "Hello World!" );
+    }
+}
