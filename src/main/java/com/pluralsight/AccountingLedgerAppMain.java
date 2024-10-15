6a1dc8443fc36ca328d7d49c56dbf311612ea1bd
Adrian <adrianchristiansanmiguel@gmail.com>
2024-10-15 13:04:24 -0700

    Added payment function

--- src/main/java/com/pluralsight/AccountingLedgerAppMain.java
+++ src/main/java/com/pluralsight/AccountingLedgerAppMain.java
@@ -1,62 +1,94 @@
 package com.pluralsight;
 
 import java.io.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class AccountingLedgerAppMain {
-    public void main(String[] args) {
-        //Home menu
-        homeMenu();
+    static Scanner scanner = new Scanner(System.in);
+    public void main () {
+        homeScreen();
+    }
+    public void homeScreen() {
+        try {
+            System.out.println("\nWelcome To Your Account Ledger Application!\n\n" +
+                    "What would you like to do today? Please enter command for selected option:\n\n" +
+                    "(L) View Ledger\n" +
+                    "(D) Make Deposit\n" +
+                    "(P) Make Payment\n" +
+                    "(X) Exit");
 
-        // make deposit
-        makeDeposit();
 
-        // make payment
-        makePayment();
+            String menuChoice = scanner.nextLine();
+            switch (menuChoice.toUpperCase()) {
+                case "L":
+                    viewLedger();
+                    break;
+                case "D":
+                    makeDeposit();
+                    break;
+                case "P":
+                    makePayment();
+                    break;
+                case "X":
+                    exit();
+                    break;
 
-        // view ledger
-        viewLedger();
-
-        // exit
-
-        // show current date and time
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd  HH:mm:ss");
-        String formattedDateTime = now.format(formatter);
-
+            }
+//           if (menuChoice.equalsIgnoreCase("L"));{
+//               getUser();
+//           } else if (menuChoice.equalsIgnoreCase("D")); {
+//                makeDeposit();
+//           } else if (menuChoice.equalsIgnoreCase("P")); {
+//               makePayment();
+//           }
+        } catch (Exception e) {
+            System.out.println("Input Error");
+            throw new RuntimeException(e);
+        }
 
     }
 
-    static Scanner scanner = new Scanner(System.in);
+    public void viewLedger() {
+        try {getUser();
+            System.out.println("Here is your current account ledger");
+          FileInputStream transactions = new FileInputStream("transactions.csv");
+          Scanner scanner = new Scanner(transactions);
+
+            String input = scanner.nextLine();
+            int lineCount = 0;
+
+            while (scanner.hasNextLine()) {
+                input = scanner.nextLine();
+                List<String> ledger = List.of(input);
+                for (int i = 0; i < ledger.size(); i++) {
+                    lineCount++;
+                    System.out.println(lineCount + ledger.get(i));
+
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Incorrect Input");
+            throw new RuntimeException(e);
+        }
+    }
+
     public void getUser() {
 
-        System.out.println("What is your Name: ");
-        String userName = scanner.nextLine();
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
 
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd  HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedDateTime = now.format(formatter);
 
-        System.out.println("Welcome " + userName + "\n\nHere is your ledger as of : " + formattedDateTime);
-
-    }
-
-    public void displayMenu() {
-        System.out.println("\nWelcome To Your Account Ledger!\n\n" +
-                "What would you like to do today? Please Enter Command For Selected Option:\n\n" +
-                "(L) View Ledger\n" +
-                "(D) Make Deposit\n" +
-                "(P) Make Payment");
-        String menuChoice = scanner.nextLine();
-        if (menuChoice.equalsIgnoreCase("L")); {
-            getUser();
-
-        }
+        System.out.println("\nHello " + name + "\n" + "User Entry Logged At: " + formattedDateTime +"\n");
 
     }
 
@@ -74,16 +106,17 @@ public class AccountingLedgerAppMain {
         try {
             FileReader fileReader = new FileReader("transactions2.csv");
             BufferedReader bufreader = new BufferedReader(fileReader);
-            FileWriter fileWriter = new FileWriter("transactions2.csv");
+            FileWriter fileWriter = new FileWriter("transactions2.csv",true);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            System.out.println("Please enter the amount of the payment : ");
-            double deposit = scanner.nextDouble();
+            System.out.println("Please enter the amount of the payment: ");
+            double payment = scanner.nextDouble();
+            scanner.nextLine();
 
-            System.out.println("Please enter a description of the payment : ");
+            System.out.println("Please enter a description of the payment: ");
             String description = scanner.nextLine();
 
-            System.out.println("Please enter the vendor name of the payment : ");
+            System.out.println("Please enter the vendor name for the payment: ");
             String vendor = scanner.nextLine();
 
 
@@ -91,42 +124,33 @@ public class AccountingLedgerAppMain {
 
             while ((input = bufreader.readLine()) != null) {
                 String[] ledger = input.split("\\|");
-                UserLedgers depositLedger = new UserLedgers(ledger[0], ledger[1], ledger[2], ledger[3], Double.parseDouble(ledger[4]));
-
+                UserLedgers paymentLedger = new UserLedgers(ledger[0], ledger[1], ledger[2], ledger[3], Double.parseDouble(ledger[4]));
+                String adjustedLedger = paymentLedger.getDate() + "|" + paymentLedger.time + "|" + description + "|" + vendor + "|" + Double.toString(payment);
+                System.out.println(adjustedLedger);
+                bufferedWriter.write(adjustedLedger);
 
 
             }
+            bufreader.close();
+            bufferedWriter.close();
 
 
         } catch (FileNotFoundException e) {
+            System.out.println("Incorrect Input");
             throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void viewLedger () {
-        try {
-            FileReader fileReader = new FileReader("transactions2.csv");
-            BufferedReader bufreader = new BufferedReader(fileReader);
-
-            String input = bufreader.readLine();
-
-            while ((input = bufreader.readLine()) != null) {
-                String[] ledger = input.split("\\|");
-                System.out.println(Arrays.toString(ledger));
-
-            }
-
-            bufreader.close();
-            } catch(FileNotFoundException e){
-                throw new RuntimeException(e);
-            } catch (IOException e) {
             System.out.println("Incorrect Input");
             throw new RuntimeException(e);
         }
     }
-    public void homeMenu () {
-        displayMenu();
+
+    public void displayMenu() {
+        homeScreen();
+    }
+
+    public void exit(){
+        System.out.println("Thank you and have a good day!");
+
     }
 }
+
--- src/main/java/com/pluralsight/UserLedgers.java
+++ src/main/java/com/pluralsight/UserLedgers.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class UserLedgers {
     String entries;
     Double deposits;
@@ -12,7 +15,7 @@ public class UserLedgers {
     Double amountChanged;
 
 
-    UserLedgers(String date, String time, String itemDescription, String vendor, Double amountChanged) {
+     public UserLedgers(String date, String time, String itemDescription, String vendor, Double amountChanged) {
         this.date = date;
         this.time = time;
         this.itemDescription = itemDescription;
@@ -23,6 +26,83 @@ public class UserLedgers {
 
     }
 
+    public String getEntries() {
+        return entries;
+    }
+
+    public void setEntries(String entries) {
+        this.entries = entries;
+    }
+
+    public Double getDeposits() {
+        return deposits;
+    }
+
+    public void setDeposits(Double deposits) {
+        this.deposits = deposits;
+    }
+
+    public Double getPayments() {
+        return payments;
+    }
+
+    public void setPayments(Double payments) {
+        this.payments = payments;
+    }
+
+    public String getReports() {
+        return reports;
+    }
+
+    public void setReports(String reports) {
+        this.reports = reports;
+    }
+
+    public String getDate() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = now.format(date);
+        return formattedDate;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedTime = now.format(time);
+        return formattedTime;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public Double getAmountChanged() {
+        return amountChanged;
+    }
+
+    public void setAmountChanged(Double amountChanged) {
+        this.amountChanged = amountChanged;
+    }
 
     public void reports() {
         System.out.println("");
